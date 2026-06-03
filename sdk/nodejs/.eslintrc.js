4d328f01bb02910599f7cbb11bbc55de77a0a35c
Jacob S Vickers <jacob_vickers@outlook.com>
2026-06-02 23:33:44 -0500

    chore: remove duplicate sdk directory to prevent code drift

--- sdk/nodejs/.eslintrc.js
+++
@@ -1,1 +0,0 @@
-module.exports = { parser: "@typescript-eslint/parser", plugins: ["@typescript-eslint"], extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"], root: true, rules: { "@typescript-eslint/no-explicit-any": "off" } };
